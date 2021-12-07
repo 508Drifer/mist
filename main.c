fa7cd72c23306dbd8d57f1d583d5b44fe8b381e1
Drifer <56264143+508Drifer@users.noreply.github.com>
2021-12-07 11:09:56 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,57 @@
-#include  "fraction.h"
+#include  "fonction.h"
 
 
-int main (){
-	Fraction fr1 = {1,3};
-	Fraction fr2 = {2,6};
-	subFraction(fr1,fr2);
-	return(0);
+int main(int argc, char *argv[]){
+  FILE *fic;
+  char c[50];
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  int i=0;
+  int A=501;
+  int j=0;
+  char tline[50];
+  fic = fopen(argv[1],"r");
 	
+	
+  if(fic==NULL){
+    printf("Erreur lors de la lecture du fichier");
+    exit(1);
+  }
+  
+
+
+  while((read = getline(&line,&len,fic)) != -1){
+    /*printf("%zu", read);*/
+
+    while(line[i]!=' '){
+
+      c[i] = line[i];
+		
+	i++;
+
+      }
+      
+     while(line[j]!='\0'){
+	      tline[j]=line[j];
+	      j=j+1;
+      }
+
+
+     if ((c[i-3]=='a')&(c[i-2]=='d')&(c[i-1]=='d')) {
+	printf("%x", add(tline,i+1));
+      } 
+      
+	
+      
+	/*if ((c[i-3]=='s')&(c[i-2]=='u')&(c[i-1]=='b')) {
+      
+	}*/
+      
+      j=0;
+      i=0;
+  }
+  return(0);
+
+
 }
